c4b299b13687f1b68cfe107fff576badbcbdc6b4
Wathsika <182477712+Wathsika@users.noreply.github.com>
2026-02-23 16:46:22 +0530

    Add web server functionality

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const fs = require("fs");
-fs.writeFile("file.txt", "Hello World!", function (err) {
-  if (err) throw err;
-  console.log("File saved!");
-});
+const http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8080);
+
+console.log("Server is running at http://localhost:8080");
