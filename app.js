bf25af6be7ac647aba7086458da303086e95f8a9
Wathsika <182477712+Wathsika@users.noreply.github.com>
2026-02-23 16:57:46 +0530

    Add async/await functionality

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const condition = true;
 
 const myPromise = new Promise((resolve, reject) => {
   if (condition) {
-    resolve("Success!");
+    resolve("Success from Async/Await!");
   } else {
     reject("Failure!");
   }
 });
 
-myPromise
-  .then((result) => {
+async function myFunction() {
+  try {
+    const result = await myPromise;
     console.log(result);
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log(error);
-  });
+  }
+}
+
+myFunction();
